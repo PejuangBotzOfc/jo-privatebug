e66427729fe50b4e898029fdf70648c37e860018
PejuangBotzOfc <114922797+PejuangBotzOfc@users.noreply.github.com>
2022-10-08 15:06:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'ᴹᴿ᭄ GuhXDོ ×፝֟͜×'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6283171691334','6283171691334','6283171691334']
+global.premium = ['6281572733164']
+global.packname = 'ᴹᴿ᭄ GuhXDོ ×፝֟͜×'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
